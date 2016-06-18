51e9d58d9ea272d469b7e54a2566903187af5b47
YuHyeon-Kim <dbgus3577@gmail.com>
2016-06-18 13:02:05 +0900

    #3 Add startActivity in MainActivity

--- app/src/main/java/kr/hs/dbgus3577e_mirim/dogguideapp/MainActivity.java
+++ app/src/main/java/kr/hs/dbgus3577e_mirim/dogguideapp/MainActivity.java
@@ -1,5 +1,6 @@
 package kr.hs.dbgus3577e_mirim.dogguideapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -8,6 +9,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        startActivity(new Intent(this, SplashActivity.class));
+
         setContentView(R.layout.activity_main);
     }
 }
